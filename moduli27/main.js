625e33b58b37679cb2b0429a3daf8f700301a06c
deon1621 <bajradeon@gmail.com>
2026-03-10 17:43:25 +0100

    moduli27

---
+++ moduli27/main.js
@@ -0,0 +1,9 @@
+function displayalert() {
+  alert(" this text is in the function")
+}
+displayalert()
+
+function mbledhja(numri1,numri2) {
+    return numri1+numri2;
+}
+document.write(mbledhja(5,8));
