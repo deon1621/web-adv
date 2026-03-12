350ffc9d5a49421b7706c419d462f59706c10e53
deon1621 <bajradeon@gmail.com>
2026-03-12 17:48:29 +0100

    moduli27

--- moduli27/main.js
+++ moduli27/main.js
@@ -1,9 +1,55 @@
-function displayalert() {
-  alert(" this text is in the function")
-}
-displayalert()
+      // function displayalert() {
+//   alert(" this text is in the function")
+// }
+// displayalert()
 
-function mbledhja(numri1,numri2) {
-    return numri1+numri2;
-}
-document.write(mbledhja(5,8));
+// function mbledhja(numri1,numri2) {
+//     return numri1+numri2;
+// }
+
+// document.write(mbledhja(5,8));
+
+// function toCelsius (f){
+//   return 5/9*(f-32);
+// }
+//  console.log(" 5 fahrenheit is equal to" +toCelsius(5)+ "celsius");
+
+//  var arrayFunction = () => alert ("Helloo")
+
+//  arrayFunction ();
+//   function dsFunction() {
+   
+//     var localvar ="Digitalschool";
+
+//     alert (localvar)
+//   }
+
+//   dsFunction();
+
+//   function toSeconds (m){
+
+//     return m*60;
+//   }
+//   console.log(toSeconds(5))
+
+
+// function toPerimeter(a,b,c){
+//   return a+b+c;
+// }
+ 
+// console.log(toPerimeter(5,4,3));
+
+
+var car = {licensPlate:"01-535-DB", vin:"bbb324462"  ,name:"BMW"  ,color:"blue", year:"2022"};
+
+alert(car.licensPlate);
+
+function computer ={name,CPU,GPU,RAM};
+
+this,name=name
+this,CPU=CPU
+this,GPU=GPU
+this,RAM=RAM
+
+var c1=new computer("8gg", "macbook","5090","intel-9")
+var c2=new computer("acer","64g","intel 9 ultra")
