519126bb8ea4351c51244ef682673a5958e55e50
deon1621 <bajradeon@gmail.com>
2026-03-26 17:47:14 +0100

    moduli30

---
+++ moduli30/main.js
@@ -0,0 +1,19 @@
+// var mosha=7;
+
+// if(mosha <8){
+    //document.write("your child is to young to learn programing")
+// };
+// else{
+    // document.write("we will call you soon")
+// };
+ 
+ var age =7;
+if(age<8){
+    document.write("your child is to young to learn programing")
+}
+else if(age >18){
+    document.write("your child is to old to learn programing")
+}
+else{
+    document.write("we will call you soon")
+}
