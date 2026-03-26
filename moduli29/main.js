08426cf94c77734c561078a3e1261765667648d9
deon1621 <bajradeon@gmail.com>
2026-03-26 17:21:05 +0100

    moduli29

--- moduli29/main.js
+++ moduli29/main.js
@@ -1,6 +1,107 @@
-var programinglangueges = ["java script, PHP, python"]
-console.log(programinglangueges);
+
+var propgrammingLanguages = ["Javascript", "PHP", "Python"];
+
+console.log(propgrammingLanguages);
 
 
-var numrat = [1,2,3,4,5,6,7,8,9,10]
-console.log(numrat)
+var numrat = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(numrat);
+
+console.log(propgrammingLanguages[1]);
+
+// array function
+
+//push add an elemente at the end of the list
+
+propgrammingLanguages.push("Java");
+
+numrat.push(11);
+
+document.write(propgrammingLanguages);
+
+document.write(numrat);
+
+//pop delete the last element
+
+numrat.pop();
+
+console.log(numrat);
+
+//unshift - add an element at the begining of the list
+
+propgrammingLanguages.unshift("C#");
+
+console.log(propgrammingLanguages);
+
+// shift - delete the first element of the list 
+
+propgrammingLanguages.shift();
+
+console.log(propgrammingLanguages);
+
+//slice - cilin pozit,sa elemente,cilin me shtu
+
+propgrammingLanguages.slice(1 , 2, "ruby")
+
+console.log(propgrammingLanguages);\
+
+// array 
+
+var students=["drini", "deon", "syari", "patrisi", "lindrit"];
+
+var students=[s1,s2,s3,s4,s5];
+
+console.log(s1)
+console.log(s2)
+console.log(s3)
+console.log(s4)
+console.log(s5)
+
+var places = [
+	"ferizaj",
+	"prishtina",
+	"peja",
+	"mitrovica",
+	"rahoveci",
+	"prizren",
+	"gjilani",
+	"istog",
+	"tokyo",
+	"moska",
+	"newyork",
+	"beijing",
+	"shanghai",
+	"LA",
+	"dallas",
+	"roma",
+	"milano",
+	"napoli",
+	"berlin",
+	"munchen"
+];
+
+
+var places=[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20]
+
+
+console.log(p1)
+console.log(p2)
+console.log(p3)
+console.log(p4)
+console.log(p5)
+console.log(p6)
+console.log(p7)
+console.log(p8)
+console.log(p9)
+console.log(p10)
+console.log(p11)
+console.log(p12)
+console.log(p13)
+console.log(p14)
+console.log(p15)
+console.log(p16)
+console.log(p17)
+console.log(p18)
+console.log(p19)
+console.log(p20)
