7fd8745cd4a26bc5b52fc89cda368c1553a2f486
deon1621 <bajradeon@gmail.com>
2026-05-12 17:45:30 +0200

    moduli36

---
+++ moduli36/main.js
@@ -0,0 +1,12 @@
+// $(window).ready(function(){
+//   alert("it is woriking")
+// })
+
+// $("#button").click(function(){
+//     console.log("the testing has beeen successfully!")
+// })
+
+
+$("#button").click(function(){
+  $("div").addClass("clicked")
+}).find('span').attr('title','hover over me')
